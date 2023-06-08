991b1b1fa8f834fe88db849f9fee5568f8956bf6
bouba-coder <boubacaryatera1@gmail.com>
2023-06-08 12:27:22 +0200

    adding consultant modifs

--- back/prisma/seeders/consultant.seeder.ts
+++ back/prisma/seeders/consultant.seeder.ts
@@ -1,36 +1,64 @@
-import { PrismaClient } from "@prisma/client";
+import { Disponibilite, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export default async function consultantSeeder() {
 
 
-    // await Promise.all(
-    //     data.map(async (consultant) => {
-    //         await prisma.consultant.upsert({
-    //             where: { email: consultant.email },
-    //             update: {},
-    //             create: consultant,
-    //         });
-    //     }
-    // ));
+  const datas = [
+    {
+      email: "createConsultantDto@gmail1.fr",
+      metier: "createConsultantDto.metier",
+      secteur: "banque",
+      disponibilite: "SIX_MOIS",
+      perimetre: "3 km",
+      competences: ['java', "angular", "nest"],
+      experiences: ['Développeur web', "Développeur information", "Développeur mobile"],
+      formations: ['Angular', "React native"],
+      actuel_entrepriseId : 1,
+      level: 1,
+      salaire: 33435,
+      userId: 1
+    },
+    {
+      email: "createConsultantDto@gmail2.fr",
+      metier: "createConsultantDto.metier",
+      secteur: "banque",
+      disponibilite: "SIX_MOIS",
+      perimetre: "3 km",
+      competences: ['java', "angular", "nest"],
+      experiences: ['Développeur web', "Développeur information", "Développeur mobile"],
+      formations: ['Angular', "React native"],
+      actuel_entrepriseId : 1,
+      level: 1,
+      salaire: 3435,
+      userId: 2
+    } 
+  ]
 
-    await prisma.consultant.upsert({
-        where: { email: "createConsultantDto.email" },
-        update: {},
-        create:     {
-            email: "createConsultantDto@gmail.fr",
-            metier: "createConsultantDto.metier",
-            secteur: "createConsultantDto.secteur",
-            disponibilite: "SIX_MOIS",
-            perimetre: "createConsultantDto.perimetre",
-            competences: ['java', "vs"],
-            experiences: ['java', "vs"],
-            formations: ['java', "vs"],
-            actuel_entrepriseId : 1,
-            level: 1,
-            salaire: 33435,
-            userId: 1
-        }
-    })
+
+await Promise.all(
+ datas.map(async (data) => {
+  await prisma.consultant.upsert({
+    where: { email: data.email },
+    update: {},
+    create: {
+      email: data.email,
+      metier: data.metier,
+      secteur: data.secteur,
+      disponibilite: Disponibilite.IMMEDIAT,
+      perimetre: data.perimetre,
+      competences: data.competences,
+      experiences: data.experiences,
+      formations: data.formations,
+      actuel_entrepriseId : data.actuel_entrepriseId,
+      level: data.level,
+      salaire: data.salaire,
+      userId: data.userId
+    },
+})
+ })
+)
+  
+
 }
--- front/src/components/ListItems.jsx
+++ front/src/components/ListItems.jsx
@@ -70,3 +70,61 @@ export const secondaryListItems = (
     </ListItemButton>
   </React.Fragment>
 );
+
+
+// consultant
+export const mainListItemsConsultant = (
+  <React.Fragment>
+    <ListItemButton>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Profil" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <LayersIcon />
+      </ListItemIcon>
+      <ListItemText primary="Mon CV" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <BarChartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Mes formations" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Forum" />
+    </ListItemButton>
+  </React.Fragment>
+);
+
+export const secondaryListItemsConsultant = (
+  <React.Fragment>
+    <ListSubheader component="div" inset>
+      Historique
+    </ListSubheader>
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Current month" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Last quarter" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Year-end sale" />
+    </ListItemButton>
+  </React.Fragment>
+);
+
