99c8d753bb1a10a11794af7c952949c619529283
Adam SELLIN <adamsellin1914@gmail.com>
2023-06-09 11:00:09 +0200

    add hooks

---
+++ front/src/hooks/useForm.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+const useForm = (initialValues, onSubmit) => {
+    const [values, setValues] = useState(initialValues);
+    const [errors, setErrors] = useState({});
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setValues({ ...values, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formErrors = validate(values);
+        setErrors(formErrors);
+
+        if (Object.keys(formErrors).length === 0) {
+        onSubmit(values);
+        }
+    };
+
+    return {
+        values,
+        errors,
+        handleChange,
+        handleSubmit,
+    };
+};
+
+export default useForm;
