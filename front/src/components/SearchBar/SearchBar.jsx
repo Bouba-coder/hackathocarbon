828fc9b04878b4901e01c04ef3e837707af27a12
bouba-coder <boubacaryatera1@gmail.com>
2023-06-11 21:31:33 +0200

    adding searcbar

--- front/src/components/SearchBar/SearchBar.jsx
+++ front/src/components/SearchBar/SearchBar.jsx
@@ -1,39 +1,68 @@
-import React, { useState, useRef, useEffect } from 'react';
-import './SearchBar.css';
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import InputBase from '@mui/material/InputBase';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 
-const SearchBar = (props) => {
-  const { placeholder } = props;
-  const [searchTerm, setSearchTerm] = useState('');
-  const inputRef = useRef(null);
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
 
-  const handleInputChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Ajoutez ici la logique pour effectuer une recherche avec le terme "searchTerm"
-    console.log('Recherche effectuée avec le terme :', searchTerm);
-  };
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'white',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
-  useEffect(() => {
-    inputRef.current.style.width = `${inputRef.current.scrollWidth}px`;
-  }, [placeholder]);
-
+export default function SearchBar() {
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="search-bar">
-        <input
-          type="text"
-          placeholder={placeholder}
-          value={searchTerm}
-          onChange={handleInputChange}
-          ref={inputRef}
-          className="custom-search-bar"
-        />
-      </div>
-    </form>
+    <Box sx={{ flexGrow: 1 }}>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Cherchez votre consultant"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+    </Box>
   );
-};
-
-export default SearchBar;
+}
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -35,45 +35,15 @@ export default function Home() {
       <CssBaseline />
       <main>
         {/* Hero unit */}
-        <Box sx={{ bgcolor: 'background.paper', pt: 8, pb: 6 }}>
-  <Container maxWidth="sm">
-    <div className="container mt-20">
-      <div className="texts">
-        <Typography component="h1" variant="h4" align="left" color="text.primary" gutterBottom>
-          Entreprise & Freelance étaient faits pour se rencontrer
-        </Typography>
-        <Typography variant="h5" align="left" color="#00BB7E" paragraph>
-          Trouvez le talent parfait pour propulser vos projets
-        </Typography>
-        <Stack direction="row" spacing={2} justifyContent="flex-start" alignItems="center">
-          <Typography variant="h6" align="left" color="text.secondary" paragraph>
-            Excellent
-          </Typography>
-          <Typography align="left" color="text.secondary" paragraph>
-            4.3 sur 5
-          </Typography>
-          <Typography variant="h6" align="left" color="text.secondary" paragraph>
-            Trustpilot
-          </Typography>
-        </Stack>
-        <Stack direction="row" spacing={2} alignItems="center" justifyContent="flex-end">
-          <SearchBar placeholder='Essayez "Java", "Consultant Marketing"...' />
-          <SearchBar placeholder='Lieu de la mission (ex: Paris, Lyon...)' />
-          <Button variant="contained">Trouvez un freelance</Button>
-          <Typography variant="h6" align="center" color="text.secondary" paragraph>
-            ou
-          </Typography>
-          <Button variant="outlined">Être Contacté</Button>
-        </Stack>
-      </div>
-      <img
-        /* src={imageHome} 
-        alt="Le signe que vous recherchiez"
-        className="image"*/
-      />
-    </div>
-  </Container>
-</Box>
+        <Box sx={{
+          bgcolor: '#282B2A',
+          pt: 10,
+          pb: 6,
+         }}>
+          <Container>
+              <SearchBar />
+          </Container>
+      </Box>
 
 
 
